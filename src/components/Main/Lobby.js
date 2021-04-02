45d16e91860115b6198d8599fdfa5b6ed3c4046e
ad <cloud179200@gmail.com>
2021-04-03 03:00:17 +0700

    add modal change avatar

--- src/components/Main/Lobby.js
+++ src/components/Main/Lobby.js
@@ -10,7 +10,13 @@ import { removeUser, switchMusic, switchSound } from "../../actions/index";
 import { useHistory } from "react-router-dom";
 import { Modal } from "semantic-ui-react";
 import { useState } from "react";
-
+import avatar1 from "../../shared/media/image/ingame_Img_user_01.png";
+import avatar2 from "../../shared/media/image/ingame_Img_user_02.png";
+import avatar3 from "../../shared/media/image/ingame_Img_user_03.png";
+import avatar4 from "../../shared/media/image/ingame_Img_user_04.png";
+import avatar5 from "../../shared/media/image/ingame_Img_user_05.png";
+import avatar6 from "../../shared/media/image/ingame_Img_user_06.png";
+import { Grid } from "semantic-ui-react";
 const openInNewTab = (url) => {
   const newWindow = window.open(url, "_blank", "noopener,noreferrer");
   if (newWindow) newWindow.opener = null;
@@ -98,6 +104,39 @@ const Lobby = () => {
     infoUser: false,
     setAvatar: false,
   });
+  const [currentDataUser, setCurrentDataUser] = useState({
+    username,
+    email,
+    gender,
+    avatar,
+  });
+  const [modalLoading, setModalLoading] = useState(false);
+
+  const getAvatarPic = () => {
+    switch (currentDataUser.avatar) {
+      case 1:
+        return avatar1;
+      case 2:
+        return avatar2;
+      case 3:
+        return avatar3;
+      case 4:
+        return avatar4;
+      case 5:
+        return avatar5;
+      case 6:
+        return avatar6;
+      default:
+        break;
+    }
+  };
+
+  const setAvatar = (ava) => {
+    setCurrentDataUser({ ...currentDataUser, avatar: ava });
+  };
+
+  const handleSubmitInfoChange = () => {};
+  const handleSubmitAvatarChange = () => {};
   return (
     <>
       <Modal open={modal.infoUser} className="modal info-user">
@@ -123,7 +162,11 @@ const Lobby = () => {
                 });
               }}
             >
-              <div></div>
+              <div
+                style={{
+                  backgroundImage: `url(${getAvatarPic()})`,
+                }}
+              ></div>
             </div>
             <div>
               <div>Email: {email}</div>
@@ -133,11 +176,11 @@ const Lobby = () => {
           </div>
           <div className="modal-username-and-id">
             <div>{username}</div>
-            <div>{id}</div>
+            <div>ID:{id}</div>
           </div>
         </div>
         <div className="modal-foot">
-          <button>Submit</button>
+          <button onClick={handleSubmitInfoChange}>Submit</button>
         </div>
       </Modal>
       <Modal open={modal.setAvatar} className="modal change-avatar">
@@ -145,12 +188,74 @@ const Lobby = () => {
           className="back-main-switch"
           onClick={() => {
             sound && playBtnClickAudio();
-            setModal({ ...modal, infoUser: !modal.infoUser, setAvatar: !modal.setAvatar });
+            setModal({
+              ...modal,
+              infoUser: !modal.infoUser,
+              setAvatar: !modal.setAvatar,
+            });
           }}
         >
           X
         </span>
         <div className="modal-head">Change your avatar</div>
+        <div className="modal-body">
+          <div className="avatar-container">
+            <div
+              name="1"
+              style={{
+                backgroundImage: `url(${avatar1})`,
+                border:
+                  currentDataUser.avatar === 1 ? "3px solid black" : "none",
+              }}
+              onClick={() => setAvatar(1)}
+            ></div>
+            <div
+              style={{
+                backgroundImage: `url(${avatar2})`,
+                border:
+                  currentDataUser.avatar === 2 ? "3px solid black" : "none",
+              }}
+              onClick={() => setAvatar(2)}
+            ></div>
+            <div
+              style={{
+                backgroundImage: `url(${avatar3})`,
+                border:
+                  currentDataUser.avatar === 3 ? "3px solid black" : "none",
+              }}
+              onClick={() => setAvatar(3)}
+            ></div>
+          </div>{" "}
+          <div className="avatar-container">
+            <div
+              style={{
+                backgroundImage: `url(${avatar4})`,
+                border:
+                  currentDataUser.avatar === 4 ? "3px solid black" : "none",
+              }}
+              onClick={() => setAvatar(4)}
+            ></div>
+            <div
+              style={{
+                backgroundImage: `url(${avatar5})`,
+                border:
+                  currentDataUser.avatar === 5 ? "3px solid black" : "none",
+              }}
+              onClick={() => setAvatar(5)}
+            ></div>
+            <div
+              style={{
+                backgroundImage: `url(${avatar6})`,
+                border:
+                  currentDataUser.avatar === 6 ? "3px solid black" : "none",
+              }}
+              onClick={() => setAvatar(6)}
+            ></div>
+          </div>
+        </div>
+        <div className="modal-foot">
+          <button onClick={handleSubmitAvatarChange}>Submit</button>
+        </div>
       </Modal>
       <div className="main-head">
         <div
@@ -160,10 +265,16 @@ const Lobby = () => {
             setModal({ ...modal, infoUser: !modal.infoUser });
           }}
         >
-          <div className="main-avatar"></div>
+          <div className="main-avatar">
+            <div
+              style={{
+                backgroundImage: `url(${getAvatarPic()})`,
+              }}
+            ></div>
+          </div>
           <div className="main-username-id">
             <div>{username}</div>
-            <div>{id}</div>
+            <div>ID:{id}</div>
           </div>
         </div>
         <div className="main-title">Seahorse WebGame</div>
