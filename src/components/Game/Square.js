10092f79213affd7ded9ad4b53c362312e3ef010
ad <cloud179200@gmail.com>
2021-04-07 01:45:52 +0700

    update-square-component

--- src/components/Game/Square.js
+++ src/components/Game/Square.js
@@ -1,12 +1,3 @@
-import blueBlock from "../../shared/media/image/square_blue_normal.png";
-import greenBlock from "../../shared/media/image/square_green_normal.png";
-import pinkBlock from "../../shared/media/image/square_pink_normal.png";
-import yellowBlock from "../../shared/media/image/square_yellow_normal.png";
-import blueBooth from "../../shared/media/image/booth_blue.png";
-import greenBooth from "../../shared/media/image/booth_green.png";
-import pinkBooth from "../../shared/media/image/booth_pink.png";
-import yellowBooth from "../../shared/media/image/booth_yellow.png";
-import finishBlock from "../../shared/media/image/finish.png";
 import { useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { addPlace } from "../../actions";
@@ -15,39 +6,6 @@ const Square = (props) => {
   const { color, type, order } = props;
   const squareRef = useRef(null);
   const dispatch = useDispatch();
-  const block = () => {
-    // eslint-disable-next-line default-case
-    switch (type) {
-      case "normal":
-        // eslint-disable-next-line default-case
-        switch (color) {
-          case "yellow":
-            return yellowBlock;
-          case "pink":
-            return pinkBlock;
-          case "green":
-            return greenBlock;
-          case "blue":
-            return blueBlock;
-        }
-        break;
-      case "booth":
-        // eslint-disable-next-line default-case
-        switch (color) {
-          case "yellow":
-            return yellowBooth;
-          case "pink":
-            return pinkBooth;
-          case "green":
-            return greenBooth;
-          case "blue":
-            return blueBooth;
-        }
-        break;
-      case "finish":
-        return finishBlock;
-    }
-  };
   useEffect(() => {
     if (squareRef) {
       const {
